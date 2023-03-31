22caeb3bb2f254d5c6ca67e4faa23fae90a6e264
Natalie Chan <nataliechan.2020@scis.smu.edu.sg>
2023-03-31 12:10:15 +0800

    User, AA w basic authorisation
    - see markdown for debugging concerns
    - don't know if authorisation is properly implemented, need review

--- attributeauthority.py
+++ attributeauthority.py
@@ -11,15 +11,15 @@ class AttributeAuthority:
         return self.pk
 
 
-    def get_decryption_key(self, gid: str, attr: str):
-        if self.users.get(gid):
+    def get_decryption_key(self, gid: str, t: str, attr: str):
+        if self.users.get("gid") == gid and self.users.get("token") == t:
             sk = RW15.keygen(self.GP, self.sk, gid, attr)
             return sk
         return None
 
 
-    def get_decryption_keys(self, gid: str, attrs):
-        if self.users.get(gid):
+    def get_decryption_keys(self, gid: str, t: str, attrs):
+        if self.users.get("gid") == gid and self.users.get("token") == t:
             sk = RW15.auth_genkeys(self.GP, self.sk, gid, attrs)
             return sk
         return None
@@ -27,20 +27,27 @@ class AttributeAuthority:
 
 def main():
     GP = RW15.global_setup('SS512')
-    Alice = {"81f5f01c-71ed-4f15-982b-f3da523e0271": {
+    Alice = {
+        "gid": "81f5f01c-71ed-4f15-982b-f3da523e0272",
         "name": "Alice",
+        "token": "asdfghjkjhgfd",
         "attributes": ["A"]
-    }}
-    Bob = {"81f5f01c-71ed-4f15-982b-f3da523e0271": {
+    }
+    Bob = {
+        "gid": "81f5f01c-71ed-4f15-982b-f3da523e0271",
         "name": "Bob",
+        "token": "wertyuiokjhgfd",
         "attributes": ["B"]
-    }}
-    users = {**Alice, **Bob}
+    }
+
+    users = {**Alice, **Bob} # only maps to Bob ????
     AA1 = AttributeAuthority('AA1', GP, users)
     AA2 = AttributeAuthority('AA2', GP, users)
     AA1pk = AA1.get_encryption_key()
-    userkey = AA1.get_decryption_key('81f5f01c-71ed-4f15-982b-f3da523e0271', 'A@AA1=1')
-    print(userkey)
+    userkeyA = AA1.get_decryption_key('81f5f01c-71ed-4f15-982b-f3da523e0272', Alice.get("token"), 'A@AA1=1')
+    userkeyB = AA1.get_decryption_key('81f5f01c-71ed-4f15-982b-f3da523e0271', Bob.get("token"), 'A@AA1=1')
+    print(userkeyA)
+    print(userkeyB)
 
 if __name__ == "__main__":
     main()
--- user.py
+++ user.py
@@ -1,54 +1,65 @@
 from rw15 import RW15
 from attributeauthority import AttributeAuthority
+import secrets as sc
 
-# WIP
-# generate and integrate auth token
+class User:
+    def __init__(self, gid: str, t: str, attrs: list): # initialise user object
+        self.gid = gid
+        self.token = t
+        self.attrs = attrs
 
-def get_keys(aid: str, u: dict, gid: str, attrs: list):
-    # get decryption keys from AA
-    GP = RW15.global_setup('SS512')
-    AA = AttributeAuthority(aid, GP, u)
+    def get_keys(self, aid: str, u: dict):  # get decryption keys from AA
+        GP = RW15.global_setup('SS512')
+        AA = AttributeAuthority(aid, GP, u)
 
-    k = AA.get_decryption_keys(gid, attrs) 
-    return k
+        # get_keys debug code
+        print("test get_keys")
+        print(self.gid)
+        print(u.get("gid"))
+        print("")
 
-def get_doc():
-    # get ciphertext from database (WIP, DO NOT RUN)
-    ct = 0
-    return ct
+        k = AA.get_decryption_keys(self.gid, self.token, self.attrs) 
+        return k
 
-def decrypt(gid: str, k: dict, ct: dict):
-    # decrypt ciphertext (WIP, DO NOT RUN)
-    GP = RW15.global_setup('SS512')
-    pt = RW15.decrypt(GP, gid, k, ct)
-    return pt
+    def get_doc(): # get ciphertext from database (WIP, DO NOT RUN)
+        ct = 0
+        return ct
+
+    def decrypt(gid: str, k: dict, ct: dict): # decrypt ciphertext (WIP, DO NOT RUN)
+        GP = RW15.global_setup('SS512')
+        pt = RW15.decrypt(GP, gid, k, ct)
+        return pt
 
 def main(): 
-    print("DEMO USE CASE 1")
+    print("DEMO USE CASE 1\n")
 
     # setup users
-    Doctor = {"D1234567@GovAA": { 
-        "attributes": ["SingHealth@GovAA","NHCS@SingHealthAA", "Doctor@SingHealthAA", "Cardiology@SingHealthAA", "Diagnosis@SingHealthAA", "Detailed@GovA", "Summary@GovAA"]
-    }}
+    dattr = ["SingHealth@GovAA","NHCS@SingHealthAA", "Doctor@SingHealthAA", "Cardiology@SingHealthAA", "Diagnosis@SingHealthAA", "Detailed@GovA", "Summary@GovAA"]
+    dt = sc.token_hex(20)
+    Doctor = User("D1234567@GovAA", dt, dattr)
 
-    Nurse = {"N6767676@GovAA": { 
-        "attributes": ["SingHealth@GovAA","NHCS@SingHealthAA", "Doctor@SingHealthAA", "Diagnosis@SingHealthAA", "Detailed@GovA", "Summary@GovAA"]
-    }}
-  
-    users = {**Doctor, **Nurse}
+    nattr = ["SingHealth@GovAA","NHCS@SingHealthAA", "Doctor@SingHealthAA", "Diagnosis@SingHealthAA", "Detailed@GovA", "Summary@GovAA"]
+    nt = sc.token_hex(20)
+    Nurse = User("N6767676@GovAA", nt, nattr)
+    
+    users = {**Doctor.__dict__, **Nurse.__dict__} # doesn't map doctor ???????
     
     # get keys from AA
-    dk = get_keys("GovAA", users, "D1234567@GovAA", Doctor.get("D1234567@GovAA").get("attributes"))
-    nk = get_keys("GovAA", users, "N6767676@GovAA", Nurse.get("N6767676@GovAA").get("attributes"))
+    dk = Doctor.get_keys("GovAA", users)
+    nk = Nurse.get_keys("GovAA", users)
+
+    # get_keys debug code
+    print("dk:", dk)
+    print("nk:", nk)
 
     # get ciphertext from database (WIP, DO NOT RUN)
-    ct = get_doc()
+    # ct = get_doc()
 
     # decrypt ciphertext (WIP, DO NOT RUN)
-    dpt = decrypt("D1234567@GovAA", dk, ct)
-    npt = decrypt("N6767676@GovAA", nk, ct)
+    # dpt = decrypt("D1234567@GovAA", dk, ct)
+    # npt = decrypt("N6767676@GovAA", nk, ct)
 
-    print("TEST END")
+    print("DEMO END")
 
 if __name__ == "__main__":
     main()
